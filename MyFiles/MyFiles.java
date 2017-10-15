2eeed754d13b0dc50162efa0e0718d501291e33e
Shyt <zzz24186@yandex.ru>
2017-10-15 17:18:54 +0400

    Редактирование

--- MyFiles/MyFiles.java
+++ MyFiles/MyFiles.java
@@ -1,17 +1,54 @@
 package MyFiles;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class MyFiles {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         //Записываем
         File myFile = new File("src/MyFiles/test2");
+        if(!myFile.exists()){ //Проверка есть ли файл
+            //myFile.mkdir(); //Создание дериктории
+            //myFile.createNewFile(); //Создание файла
+        }
+        if(myFile.isDirectory()){
+            System.out.println("Проверяем это дериктория?");
+        }
+        if(myFile.isFile()){
+            System.out.println("Проверяем это Фаил");
+        }
+
+        //Записывать
+        FileWriter fileWriter = new FileWriter(myFile);
+        fileWriter.write("str1\n");
+        fileWriter.write("str2\n");
+        fileWriter.flush(); //Записываем
+        //fileWriter.close(); //Закрываем
+
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write("str3");
+        bufferedWriter.newLine(); //Перенос на новую строку вместо \n
+        bufferedWriter.write("str4");
+        bufferedWriter.flush();
+        bufferedWriter.close();
+
+        //Читать
+        FileReader fileReader = new FileReader(myFile);
+        //char[] chars = new char[20];
+        //fileReader.read(chars);
+        //System.out.println(chars);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        while (bufferedReader.ready()){
+            System.out.println(bufferedReader.readLine()); //Читаем по строчно
+        }
+
+
+
+
+/*
         PrintWriter pw = new PrintWriter(myFile); //Он может записывать только текст, но не байты информации
         pw.println("5 6 7 8");
         pw.close();
@@ -29,11 +66,12 @@ public class MyFiles {
         }
         System.out.println(Arrays.toString(myNambers));
 
-        /*
-        while(myScanner.hasNextLine()){
-            System.out.println(myScanner.nextLine()); //Массив строк в текстовом файле
-        }
-        */
+
+        //while(myScanner.hasNextLine()){
+            //System.out.println(myScanner.nextLine()); //Массив строк в текстовом файле
+        //}
+
         myScanner.close();
+*/
     }
 }
