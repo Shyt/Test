7b13eaaa7405555b3781c4b90f86f91f984bafa3
Shyt <zzz24186@yandex.ru>
2017-10-15 17:18:44 +0400

    Редактирование

--- MyDate/MyDate.java
+++ MyDate/MyDate.java
@@ -1,5 +1,8 @@
 package MyDate;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -7,9 +10,11 @@ import java.util.GregorianCalendar;
 public class MyDate {
     public static void main(String[] args){
 
+        //Date
         System.out.println(new Date()); //Mon Oct 09 10:09:22 GMT+04:00 2017
         System.out.println(new Date().toString());
 
+        //Calendar
         GregorianCalendar myNow1 = new GregorianCalendar();
         int myMonth1 = myNow1.get(Calendar.MONTH);
         int myWeekday1 = myNow1.get(Calendar.DAY_OF_WEEK);
@@ -24,6 +29,21 @@ public class MyDate {
 
         //add, set - Методы модифицируещие
         //get - Метод доступа
+
+        //DataFormat
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT); //Общий формат, new не указывается
+        System.out.println(dateFormat.format(myNow2.getTime()));
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        System.out.println(simpleDateFormat.format(myNow2.getTime()));
+
+        //Обратное преобразование
+        try {
+            Date newDate = simpleDateFormat.parse("23/05/1964 05:22");
+            System.out.println(newDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 }
