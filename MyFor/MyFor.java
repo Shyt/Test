b0d9dc3ea840d27277447c6054f59165b63671c6
Shyt <zzz24186@yandex.ru>
2017-10-15 17:18:59 +0400

    Редактирование

--- MyFor/MyFor.java
+++ MyFor/MyFor.java
@@ -7,6 +7,11 @@ public class MyFor {
             if(i%2 == 0){
                 continue; //Продолжает выполнять но после него нечего не обрабатывается
             }
+            //System.out.println(i);
+        }
+
+        int[] myArray = {1,2,3};
+        for (int i: myArray){
             System.out.println(i);
         }
     }
